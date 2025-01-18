5326ac2771e2b16869056fc3e30d5e8e85c6cb53
shuo xu <shuo.xu@2021.icam.fr>
2025-01-18 10:57:06 +0800

    JS update: get lyrics

--- script.js
+++ script.js
@@ -56,6 +56,24 @@ async function getMoreSongs(url) {
     showData(data);
 }
 
+
+// Get lyrics for a song
+async function getLyrics(artist, songTitle) {
+    const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
+    const data = await res.json();
+    // console.log(data);
+
+    // r = return, n = newline, g = global scope search
+    // if any letter r and letter n, or, letter r, or letter n, has been found in the global scope, replace them with a break 
+
+    const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
+
+    result.innerHTML = `<h2><strong>${artist}</strong> - ${songTitle}</h2>
+    <span>${lyrics}</span>`;
+
+    more.innerHTML = ''; 
+}
+
 // Event listeners
 form.addEventListener('submit', e => {
     e.preventDefault();
@@ -68,3 +86,15 @@ form.addEventListener('submit', e => {
         searchSongs(searchTerm); 
     }
 }); 
+
+//Get lyrics button click
+result.addEventListener('click', e => {
+    const clickedEl = e.target; 
+
+    if (clickedEl.tagName === 'BUTTON') {
+        const artist = clickedEl.getAttribute('data-artist');
+        const songTitle = clickedEl.getAttribute('data-songtitle');
+
+        getLyrics(artist, songTitle);
+    }
+});
